99e5915f5d149b34d5a09aa7a153223c6a17ed1e
vishal77-dev <56531230+vishal77-dev@users.noreply.github.com>
2022-09-26 12:45:03 +0530

    Update powerBiDashboardAuraHelper.js

--- SalesforceAppOwnsDataEmbedding/force-app/main/default/aura/powerBiDashboardAura/powerBiDashboardAuraHelper.js
+++ SalesforceAppOwnsDataEmbedding/force-app/main/default/aura/powerBiDashboardAura/powerBiDashboardAuraHelper.js
@@ -8,12 +8,12 @@
         );
     
         
-        workspaceIdIsValidGuid = pattern1.test(ReportworkspaceId);
-        reportIdIsValidGuid = pattern1.test(eventReportId);
+        workspaceIdIsValidGuid1 = pattern1.test(ReportworkspaceId);
+        reportIdIsValidGuid1 = pattern1.test(eventReportId);
     
         console.log("afterScriptsLoaded 6");
     
-        if (!workspaceIdIsValidGuid || !reportIdIsValidGuid) {
+        if (!workspaceIdIsValidGuid1 || !reportIdIsValidGuid1) {
           component.set(
             "v.errorMessage1",
             "Workspace ID and Report ID must be configured as valid GUID!"
@@ -23,42 +23,42 @@
           console.log("Embedding Report");
           component.set("v.errorMessage1", undefined);
     
-          var action = component.get("c.getEmbeddingDataForReport");
+          var action1 = component.get("c.getEmbeddingDataForReport");
     
           console.log("Embedding Report 2");
     
-          action.setParams({
+          action1.setParams({
             WorkspaceId: ReportworkspaceId,
             ReportId: eventReportId
           });
     
           console.log("Embedding Report 3");
     
-          action.setCallback(this, function (response) {
+          action1.setCallback(this, function (response1) {
             console.log("callback executing");
-            var state = response.getState();
-            if (state === "SUCCESS") {
+            var state1 = response1.getState();
+            if (state1 === "SUCCESS") {
               console.log("callback success");
     
-              var embeddingData = response.getReturnValue();
+              var embeddingData1 = response1.getReturnValue();
     
-              console.log("embeddingData", embeddingData);
+              console.log("embeddingData", embeddingData1);
     
-              if(embeddingData.error){
-                component.set("v.errorMessage1", embeddingData.error);
+              if(embeddingData1.error){
+                component.set("v.errorMessage1", embeddingData1.error);
                 component.set("v.ShowDashboard",false);
               }
     
-              if(embeddingData.embedUrl){
-                var reportId = embeddingData.reportId;
-                var embedUrl = embeddingData.embedUrl;
-                var token = embeddingData.embedToken;
+              if(embeddingData1.embedUrl){
+                var reportId1 = embeddingData1.reportId;
+                var embedUrl1 = embeddingData1.embedUrl;
+                var token1 = embeddingData1.embedToken;
       
-                var config = {
+                var config1 = {
                   type: "report",
-                  id: reportId,
-                  embedUrl: embedUrl,
-                  accessToken: token,
+                  id: reportId1,
+                  embedUrl: embedUrl1,
+                  accessToken: token1,
                   tokenType: 1,
                   settings: {
                     panes: {
@@ -70,10 +70,10 @@
       
                 // Embed the report and display it within the div container.
                 component.set("v.ShowDashboard",false);
-                var embedContainer = component.find("embed-container1").getElement();
-                var report = powerbi.embed(embedContainer, config);
+                var embedContainer1 = component.find("embed-container1").getElement();
+                var report1 = powerbi.embed(embedContainer1, config1);
       
-                console.log(report);
+                console.log(report1);
       
               }
             }
@@ -81,7 +81,7 @@
     
           console.log("Embedding Report 4");
     
-          $A.enqueueAction(action);
+          $A.enqueueAction(action1);
     
           console.log("Embedding Report 4");
         }
